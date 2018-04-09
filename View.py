06f96c00607c3babac8440b96427436684374161
evinceng <aslan.evin@gmail.com>
2018-04-09 12:25:53 +0200

    slider added to view

--- View.py
+++ View.py
@@ -11,13 +11,21 @@ class View():
     def __init__(self, master):
         self.frame = Tk.Frame(master)
         self.frame.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
+        self.mainpanel = MainPanel(master)
         self.sidepanel=SidePanel(master)
  
+class MainPanel():
+     def __init__(self, root):
+        self.frame2 = Tk.Frame(root)
+        self.frame2.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
+        self.slider = Tk.Scale(self.frame2, from_ = 0, to_ = 1, orient=Tk.HORIZONTAL, resolution = 0.001)
+        self.slider.pack()
+        
 class SidePanel():
     def __init__(self, root):
-        self.frame2 = Tk.Frame( root )
-        self.frame2.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
-        self.startBut = Tk.Button(self.frame2, text="Start ")
+        self.frame3 = Tk.Frame(root)
+        self.frame3.pack(side=Tk.RIGHT, fill=Tk.BOTH, expand=1)
+        self.startBut = Tk.Button(self.frame3, text="Start ")
         self.startBut.pack(side="top",fill=Tk.BOTH)
-        self.stopButton = Tk.Button(self.frame2, text="Stop")
+        self.stopButton = Tk.Button(self.frame3, text="Stop")
         self.stopButton.pack(side="top",fill=Tk.BOTH)
